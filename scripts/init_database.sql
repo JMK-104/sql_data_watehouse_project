796a5f7945e3be8b80e19280aafeea3c632422e9
Justin Kakuyo <justin.mk.1004@gmail.com>
2025-09-12 18:33:53 +0300

    Create init_database.sql
    Create a new database 'data_warehouse' and schemas: 'bronze', 'silver', and 'gold'

---
+++ scripts/init_database.sql
@@ -0,0 +1,30 @@
+/*
+===========================
+Create Database and Schemas
+===========================
+
+Script Purpose:
+    This script creates a new database 'data_warehouse' after dropping an already existing one 
+    that might share the same name. If one exists already, it is dropped and recreated. Additionally,
+    the script creates three new schemas within the database: 'bronze', 'silver', and 'gold'
+
+WARNING:
+    Running this script will drop any existing database named 'data_warehouse' by default. All data in such a database
+    will be permanently deleted. It is highly recommended to choose a new name for database creation
+    to avoid data deletion and overwriting. Please proceed with caution and ensure your existing data is
+    backed up where necessary before running this script.
+*/
+
+-- Create a new database called 'data_warehouse'
+DROP DATABASE IF EXISTS data_warehouse; -- Use appropriate name if necessary
+CREATE DATABASE data_warehouse; -- Use appropriate name if necessary
+
+-- Multiple schemas needed: Bronze, Silver, Gold
+-- Create bronze schema
+CREATE SCHEMA bronze;
+
+-- Create silver schema
+CREATE SCHEMA silver;
+
+-- Create gold schema
+CREATE SCHEMA gold;
