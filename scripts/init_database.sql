5e21ecbc2df09e0981b1979024190426623fd42a
Justin Kakuyo <justin.mk.1004@gmail.com>
2025-09-23 17:21:16 +0300

    Update init_database.sql
    add etl schema for full etl stored procedure

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,17 +4,17 @@ Create Database and Schemas
 ===========================
 
 Script Purpose:
-    This script creates a new database 'data_warehouse' after dropping an already existing one 
-    that might share the same name. If one exists already, it is dropped and recreated. Additionally,
-    the script creates three new schemas within the database: 'bronze', 'silver', and 'gold'
+This script creates a new database 'data_warehouse' after dropping an already existing one 
+which might share the same name. If one exists already, it is dropped and recreated. Additionaly,
+the script creates three new schemas within the database: 'bronze', 'silver', and 'gold'
 
 WARNING:
-    Running this script will drop any existing database named 'data_warehouse' by default. All data in such a database
-    will be permanently deleted. It is highly recommended to choose a new name for database creation
-    to avoid data deletion and overwriting. Please proceed with caution and ensure your existing data is
-    backed up where necessary before running this script.
-*/
-
+Running this script will drop any existing database named 'data_warehouse' by default. All data in such a database
+will be permanently deleted. It is highly recommended to choose a new name for database creation
+to avoid data deletion and overwrite. Please proceed with caution and ensure your existing data is
+backed up where necessary before running this script.
+ */
+-- Create new database called 'data_warehouse'
 DROP DATABASE IF EXISTS data_warehouse
 ;
 
@@ -30,7 +30,6 @@ CREATE DATABASE data_warehouse
 CREATE SCHEMA bronze
 ;
 
-
 -- Create silver schema
 CREATE SCHEMA silver
 ;
@@ -38,4 +37,8 @@ CREATE SCHEMA silver
 
 -- Create gold schema
 CREATE SCHEMA gold
+;
+
+-- Create etl schema for procedure
+CREATE SCHEMA etl
 ;
